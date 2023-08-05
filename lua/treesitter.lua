205322245b1c78ec40c5f32b6de8940827eacf02
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-08-04 22:45:10 -0300

    remove olders

--- lua/treesitter.lua
+++
@@ -1,8 +0,0 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = '',
-  highlight = {enable = true},
-  indent = {enable = true}
-}
-
-local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-parser_config.tsx.filetype_to_parsername = {'javascript', 'typescript.tsx'}
